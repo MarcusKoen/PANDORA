e6d7e7ac662d5ef79b74abd545a3f94b3289dd4c
MarcusKoen <37168149+MarcusKoen@users.noreply.github.com>
2025-07-22 19:35:55 +0200

    Update main_revised.cpp

--- 1.Simple Passcode stored statically/main_revised.cpp
+++ 1.Simple Passcode stored statically/main_revised.cpp
@@ -3,16 +3,30 @@ using namespace std;
 
 int main()
 {
-    int code = 8008;
+    int code = 80085;
     int test = 0;
-    cout << "Please enter the code" << endl;
+    char choice;
 
-    while(test != code)
-    {
-        cin >> test;
-        cout << "Wrong code!" << endl;
-    }
-        cout << "Congrats" << endl;
+    do {
+        cout << "Please enter the code" << endl;
+        bool correct = false;
 
+        while (!correct) {
+            cin >> test;
+
+            if (test != code) {
+                cout << "Wrong code!" << endl;
+            } else {
+                cout << "Congrats" << endl;
+                correct = true;
+            }
+        }
+
+        cout << "Do you want to try again? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    cout << "Program exiting..." << endl;
     return 0;
 }
